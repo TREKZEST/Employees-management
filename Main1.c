85c13a6c2e8a7fbf3cb1386009f6fb1260289c74
Saurabh <ashgamming806@gmail.com>
2024-11-30 19:46:01 +0530

    update Main1.c

--- Main1.c
+++ Main1.c
@@ -1,219 +1,173 @@
-// C Program to implement Bus Reservation System
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+// including necessary header files
 
-// Define a structure to store bus information
-struct Bus{
-    int busNumber;
-    char source[50];
-    char destination[50];
-    int totalSeats;
-    int availableSeats;
-    float fare;
-};
+#define MAX_C 11
+// defining max number of candidates
 
-// Define a structure to store user login information
-struct User{
-    char username[50];
-    char password[50];
-};
+// Structure of candidate
+typedef struct Candidate {
+    char name[50];
+    int votes;
+    char symbol;
+} Candidate;
 
-// Function to display the main menu
-void displayMainMenu(){
-    printf("\n=== Main Menu ===\n");
-    printf("1. Login\n");
-    printf("2. Exit\n");
-    printf("Enter your choice: ");
-}
+// global array of candidate details
+Candidate allCandidates[MAX_C];
+// global variable to keep the cound of candidates
+int candidateCount = 0;
+// global array to store all symbols
+char symbols[10]
+    = { '!', '@', '#', '$', '%', '^', '&', '*', '~', '+' };
+// array to keep track of taken symbol
+int symbolTaken[11];
 
-// Function to display the user menu
-void displayUserMenu(){
-    printf("\n=== User Menu ===\n");
-    printf("1. Book a Ticket\n");
-    printf("2. Cancel a Ticket\n");
-    printf("3. Check Bus Status\n");
-    printf("4. Logout\n");
-    printf("Enter your choice: ");
-}
+// function declaration
+void fillCandidate(int);
+void displayAllCandidates();
+void getVotes(int);
+void getResults();
 
-// Function to perform user login
-int loginUser(struct User users[], int numUsers, char username[], char password[]){
-    for (int i = 0; i < numUsers; i++){
-        if (strcmp(users[i].username, username) == 0 && strcmp(users[i].password, password) == 0){
-            return i; // Return the index of the logged-in user
-        }
-    }
-    return -1; // Return -1 if login fails
-}
-
-// Function to book tickets
-void bookTicket(struct Bus buses[], int numBuses){
-    printf("\nEnter Bus Number: ");
-    int busNumber;
-    scanf("%d", &busNumber);
-
-    // Find the bus with the given busNumber
-    int busIndex = -1;
-    for (int i = 0; i < numBuses; i++){
-        if (buses[i].busNumber == busNumber){
-            busIndex = i;
-            break;
-        }
+// driver code
+int main()
+{
+    // initializing necessary data structures
+    for (int i = 0; i < 11; i++) {
+        symbolTaken[i] = 0;
     }
 
-    if (busIndex == -1){
-        printf("Bus with Bus Number %d not found.\n", busNumber);
-    }
-    else{
-        printf("Enter Number of Seats: ");
-        int seatsToBook;
-        scanf("%d", &seatsToBook);
-
-        if (buses[busIndex].availableSeats < seatsToBook){
-            printf("Sorry, only %d seats are available.\n", buses[busIndex].availableSeats);
-        }
-        else{
-            buses[busIndex].availableSeats -= seatsToBook;
-            printf("Booking successful! %d seats booked on Bus Number %d.\n", seatsToBook, busNumber);
-        }
-    }
-}
-
-// Function to cancel tickets
-void cancelTicket(struct Bus buses[], int numBuses){
-    printf("\nEnter Bus Number: ");
-    int busNumber;
-    scanf("%d", &busNumber);
-
-    // Find the bus with the given busNumber
-    int busIndex = -1;
-    for (int i = 0; i < numBuses; i++){
-        if (buses[i].busNumber == busNumber){
-            busIndex = i;
-            break;
-        }
+    // getting the number of candidates
+    printf("Enter the number of candidates: ");
+    scanf("%d", &candidateCount);
+    if (candidateCount >= MAX_C) {
+        printf("Number of candidates cannot be greater "
+               "than 10.\n Terminating the program\n\n");
+        return 0;
     }
 
-    if (busIndex == -1){
-        printf("Bus with Bus Number %d not found.\n", busNumber);
-    }
-    else{
-        printf("Enter Number of Seats to Cancel: ");
-        int seatsToCancel;
-        scanf("%d", &seatsToCancel);
-
-        if (seatsToCancel > (buses[busIndex].totalSeats - buses[busIndex].availableSeats)){
-            printf("Error: You can't cancel more seats than were booked.\n");
-        }
-        else{
-            buses[busIndex].availableSeats += seatsToCancel;
-            printf("Cancellation successful! %d seats canceled on Bus Number %d.\n", seatsToCancel,
-                   busNumber);
-        }
-    }
-}
-
-// Function to check bus status
-void checkBusStatus(struct Bus buses[], int numBuses){
-    printf("\nEnter Bus Number: ");
-    int busNumber;
-    scanf("%d", &busNumber);
-
-    // Find the bus with the given busNumber
-    int busIndex = -1;
-    for (int i = 0; i < numBuses; i++){
-        if (buses[i].busNumber == busNumber){
-            busIndex = i;
-            break;
-        }
+    // filling the details of the candidate
+    for (int i = 0; i < candidateCount; i++) {
+        fillCandidate(i);
     }
 
-    if (busIndex != -1){
-        printf("\nBus Number: %d\n", buses[busIndex].busNumber);
-        printf("Source: %s\n", buses[busIndex].source);
-        printf("Destination: %s\n", buses[busIndex].destination);
-        printf("Total Seats: %d\n", buses[busIndex].totalSeats);
-        printf("Available Seats: %d\n", buses[busIndex].availableSeats);
-        printf("Fare: %.2f\n", buses[busIndex].fare);
+    // getting the number of voters
+    int numVoters;
+    printf("Enter the number of voters: ");
+    scanf("%d", &numVoters);
+
+    // Collecting votes
+    for (int i = 0; i < numVoters; i++) {
+        getVotes(i);
     }
-    else{
-        printf("Bus with Bus Number %d not found.\n", busNumber);
-    }
-}
 
-int main(){
-    // Initialize user data
-    struct User users[5] = {
-        {"user1", "pass1"}, {"user2", "pass2"}, {"user3", "pass3"}, {"user4", "pass4"}, {"user5", "pass5"},
-    };
-    int numUsers = 5;
-
-    // Initialize bus data
-    struct Bus buses[3] = {
-        {101, "City A", "City B", 50, 50, 500.0},
-        {102, "City C", "City D", 40, 40, 400.0},
-        {103, "City E", "City F", 30, 30, 300.0},
-    };
-    int numBuses = 3;
-
-    int loggedInUserId = -1; // Index of the logged-in user
-
-    while (1){
-        if (loggedInUserId == -1){
-            displayMainMenu();
-            int choice;
-            scanf("%d", &choice);
-
-            if (choice == 1){
-                char username[50];
-                char password[50];
-
-                printf("Enter Username: ");
-                scanf("%s", username);
-                printf("Enter Password: ");
-                scanf("%s", password);
-
-                loggedInUserId = loginUser(users, numUsers, username, password);
-                if (loggedInUserId == -1){
-                    printf("Login failed. Please check your username and password.\n");
-                }
-                else{
-                    printf("Login successful. Welcome, %s!\n", username);
-                }
-            }
-            else if (choice == 2){
-                printf("Exiting the program.\n");
-                break;
-            }
-            else{
-                printf("Invalid choice. Please try again.\n");
-            }
-        }
-        else{
-            displayUserMenu();
-            int userChoice;
-            scanf("%d", &userChoice);
-
-            switch (userChoice){
-            case 1:
-                bookTicket(buses, numBuses);
-                break;
-            case 2:
-                cancelTicket(buses, numBuses);
-                break;
-            case 3:
-                checkBusStatus(buses, numBuses);
-                break;
-            case 4:
-                printf("Logging out.\n");
-                loggedInUserId = -1;
-                break;
-            default:
-                printf("Invalid choice. Please try again.\n");
-            }
-        }
-    }
+    // printing results
+    getResults();
 
     return 0;
+}
+
+// function to populate the allCandidates array using the
+// details provided by user
+void fillCandidate(int cNum)
+{
+    printf("Available Symbols: \n");
+    for (int j = 0; j < 10; j++) {
+        if (symbolTaken[j] == 1)
+            continue;
+        printf("%d  %c\n", j + 1, symbols[j]);
+    }
+
+    int num = 0;
+
+    printf("\nEnter the symbol number of candidate %d: ",
+           cNum + 1);
+    scanf("%d", &num);
+
+    if (num <= 0 || num > 10 || symbolTaken[num - 1] == 1) {
+        printf("This Symbol is not available. Please "
+               "choose from the available symbols\n");
+        num = 0;
+        fillCandidate(cNum);
+    }
+    else {
+        symbolTaken[num - 1] = 1;
+        allCandidates[cNum].symbol = symbols[num - 1];
+        printf("Enter the name of candidate %d: ",
+               cNum + 1);
+        scanf("%s", allCandidates[cNum].name);
+
+        allCandidates[cNum].votes = 0;
+    }
+}
+
+// function to display all candidates name with symbol
+void displayAllCandidates()
+{
+    if (!allCandidates || !candidateCount) {
+        perror("Invalid Candidate Array\n");
+        return;
+    }
+
+    for (int j = 0; j < candidateCount; j++) {
+        printf("%s\t\t", allCandidates[j].name);
+    }
+    printf("\n");
+    for (int j = 0; j < candidateCount; j++) {
+        printf("%3c\t\t\t", allCandidates[j].symbol);
+    }
+    printf("\n");
+}
+
+// function to get votes
+void getVotes(int voterCount)
+{
+    displayAllCandidates();
+    printf("Voter %d, please enter your choice (1-%d): ",
+           voterCount + 1, candidateCount);
+    int choice;
+    scanf("%d", &choice);
+
+    // checking for valid choice
+    if (choice >= 1 && choice <= candidateCount) {
+        allCandidates[choice - 1].votes++;
+    }
+    else {
+        printf("Invalid choice! Please vote again.\n");
+        getVotes(voterCount);
+    }
+}
+
+// function to get results
+void getResults()
+{
+    int maxVotes = 0;
+    int winnerIndex = -1;
+    int winnerFrequency = 0;
+    for (int i = 0; i < candidateCount; i++) {
+        if (allCandidates[i].votes > maxVotes) {
+            maxVotes = allCandidates[i].votes;
+            winnerIndex = i;
+        }
+    }
+
+    for (int i = 0; i < candidateCount; i++) {
+        if (allCandidates[i].votes == maxVotes) {
+            winnerFrequency++;
+        }
+    }
+
+    printf("\n-----RESULT-----\n");
+
+    if (winnerFrequency > 1) {
+        printf("No candidate has majority votes\n");
+    }
+    else if (winnerIndex != -1) {
+        printf("The winner is: %s\nCandidate Symbol: "
+               "%c\nwith %d votes!\n",
+               allCandidates[winnerIndex].name,
+               allCandidates[winnerIndex].symbol, maxVotes);
+    }
+    else {
+        printf("No winner\n");
+    }
 }
